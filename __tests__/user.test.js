0bf88882582afeda813636bc88bc524d845ee8e9
Joseph Brown <84300225+Joseph-K-B@users.noreply.github.com>
2022-01-12 15:06:32 -0800

    auth tests passing

---
+++ __tests__/user.test.js
@@ -0,0 +1,89 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+const UserService = require('../lib/services/AuthService');
+
+const testUser = {
+  email: 'test@email.com',
+  password: 'password'
+};
+
+const badUser = {
+  email: 'badtest@email.com',
+  password: 'password'
+};
+
+describe('crud for user', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+
+  it('posts new user to DB', async () => {
+    const res = await request(app)
+    .post('/api/v1/auth/signup')
+    .send({
+      email: 'test@email.com',
+      password: 'password',
+    });
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      email: 'test@email.com'
+    });
+  });
+
+  it('logs existing user in via post route', async () => {
+    await UserService.createUser(testUser);
+    const res = await request(app)
+      .post('/api/v1/auth/login')
+      .send(testUser);
+      expect(res.body).toEqual({
+        id: expect.any(String),
+        email: 'test@email.com'
+      });
+  });
+
+  it('ensures login credentials exist', async () => {
+    await UserService.createUser(testUser);
+    
+    const res = await request(app)
+    .post('/api/v1/auth/login')
+    .send(badUser);
+    
+    expect(res.body).toEqual({
+      message: 'Invalid email/password',
+      status: 401
+    });
+  });
+  
+  it('gets currently logged user at /profile', async () => {
+    await UserService.createUser({
+      email: 'test@email.com',
+      password: 'password',
+    });
+    
+    const agent = request.agent(app);
+    
+    await agent
+    .post('/api/v1/auth/login')
+    .send({
+      email: 'test@email.com',
+      password: 'password',
+    })
+    
+    const res = await agent
+    .get('/api/v1/auth/profile');
+    
+    console.log(res.body);
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      exp: expect.any(Number),
+      iat: expect.any(Number),
+      email: 'test@email.com'
+    });
+  });
+
+  afterAll(() => {
+    pool.end();
+  });
+});
