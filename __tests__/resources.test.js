fcc36f4c112945a46b1280e8c237c6a50af65202
Joseph Brown <84300225+Joseph-K-B@users.noreply.github.com>
2022-01-16 23:26:57 -0800

    mostly auth changes, updated topics for tests, worked on resources routes as mob

--- __tests__/resources.test.js
+++ __tests__/resources.test.js
@@ -2,7 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
-const Resources = require('../lib/models/Resources');
+// const Resources = require('../lib/models/Resources');
 
 const insertResource = {
   resourceName: 'example inc.',
