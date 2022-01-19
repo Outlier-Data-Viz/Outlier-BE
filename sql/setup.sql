5de8ec06c1c21961373787f6b080a3874f699b3d
maddenlockin <maddenlockin@gmail.com>
2022-01-18 20:35:51 -0800

    resources seeded with fixes to state abreviations

--- sql/setup.sql
+++ sql/setup.sql
@@ -3,14 +3,8 @@ DROP TABLE IF EXISTS users CASCADE;
 DROP TABLE IF EXISTS states CASCADE;
 DROP TABLE IF EXISTS populations CASCADE;
 DROP TABLE IF EXISTS topics CASCADE;
-<<<<<<< HEAD
 DROP TABLE IF EXISTS favorites CASCADE;
 DROP TABLE IF EXISTS resources CASCADE; 
-=======
--- DROP TABLE IF EXISTS additional_data CASCADE;
-DROP TABLE IF EXISTS favorites;
-DROP TABLE IF EXISTS resources; 
->>>>>>> 1059d386134f3ac3212510292ef23020cbf2d9e0
 
 CREATE TABLE auth (
   id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
@@ -132,11 +126,8 @@ INSERT INTO states (state_name, abrv) VALUES ('alabama', 'AL'),
 
 
 INSERT INTO topics(name)
-VALUES ('interesting');
+VALUES ('Homelessness'), ('Food');
 
-
-INSERT INTO resources(resource_name, resource_URL, state_abrv, topics_id)
-VALUES ('glbtays', 'www.glbtays.org', 'AL', '1');
 
 -- INSERT INTO populations(state_abrv, total, lgbt, aa, latinx)
 -- VALUES ('AL', 4903185, 117000, 1296162, 264067),
@@ -242,3 +233,86 @@ VALUES ('AL', 4903185, 117000, 1296162, 264067, 3351, null),
 ('WI', 5822434, 171000, 376256, 447290, 4515, 18900),
 ('WY', 578759, 15000, 5232, 59046, 612, 2600);
 
+INSERT INTO resources(resource_name, resource_url, state_abrv, topics_id)
+VALUES
+('Free Food- by region', 'https://www.freefood.org', 'AZ', '2'),
+('Homeless to Independence', 'https://www.homelesstoindependence.org/alabama/alabama-emergency-services/', 'AL', '1'),
+('IDENTITY, INC', 'https://identityalaska.org/', 'AK', '1'),
+('Alaska Housing Finance Corporation', 'https://www.ahfc.us/', 'AK', '1'),
+('DES Utility Assistance ', 'https://des.az.gov/services/basic-needs/shelter-housing/utility-assistance', 'AZ', '1'),
+('DES Homeless Services', 'https://des.az.gov/services/basic-needs/shelter-housing/homeless-services', 'AZ', '1'),
+('Arkansas Hunger Relief Alliance', 'https://arhungeralliance.org/emergency-resources/', 'AR', '1'),
+('Arkansas Department of Human Services: Programs', 'https://humanservices.arkansas.gov/learn-about-programs/', 'AR', '1'),
+('Project Roomkey', 'https://www.cdss.ca.gov/inforesources/cdss-programs/housing-programs/project-roomkey', 'CA', '1'),
+('CDSS Housing and Homelessness Programs', 'https://www.cdss.ca.gov/inforesources/cdss-programs/housing-programs', 'CA', '1'),
+('COVID19 Housing and Rent Help', 'https://covid19.ca.gov/housing-and-homelessness/#People-experiencing-homelessness', 'CA', '1'),
+('Colorado Coalition for the Homeless', 'https://www.coloradocoalition.org/gethelp', 'CO', '1'),
+('CT Health and Human Services', 'https://portal.ct.gov/Services/Health-and-Human-Services', 'CT', '1'),
+('Delaware Health and Social Services', 'https://dhss.delaware.gov/dhss/', 'DE', '1'),
+('Florida Coalition to End Homelessness', 'http://fchonline.org/resources/', 'FL', '1'),
+('Georgia Department of Community Affairs: Food', 'https://www.dca.ga.gov/safe-affordable-housing/homeless-special-needs-housing/covid-19-resources/food', 'GA', '1'),
+('University of Hawaii at Manoa: List of Local Organizations Addressing Homelessness', 'https://guides.library.manoa.hawaii.edu/c.php?g=105635&p=684598', 'HI', '1'),
+('Idaho Housing and Finance Association', 'https://www.idahohousing.com/homelessness-services-programs/', 'ID', '1'),
+('Jesse Tree', 'https://www.jessetreeidaho.org/', 'ID', '1'),
+('Illinois Department of Human Services: Housing, Shelter, and Homelessness Resources', 'https://www.dhs.state.il.us/page.aspx?item=124295', 'IL', '1'),
+('Coalition for Homelessness Intervention and Prevention: Handbook of Help', 'http://handbookofhelp.org/', 'IN', '1'),
+('Shelter House Iowa', 'https://shelterhouseiowa.org/programs-and-services/', 'IA', '1'),
+('Iowa Finance Authority', 'https://www.iowafinance.com/homelessness/', 'IA', '1'),
+('KUMC JayDoc Free Clinic: Community Resources', 'https://www.kumc.edu/school-of-medicine/patient-care/student-run-clinics/jaydoc-free-clinic/resources/community-assistance-resources.html', 'KS', '1'),
+('KC Footprints for Homeless Veterans', 'https://www.kcfootprints.org/', 'KS', '1'),
+('KC Footprints For Homeless Veterans', 'https://www.kcfootprints.org/', 'MO', '1'),
+('Sheffield Place', 'https://www.sheffieldplace.org/programs', 'MO', '1'),
+('HHCK: Housing Programs', 'https://www.hhck.org/housing-programs', 'KY', '1'),
+('Louisiana BOSCO: Housing and Services', 'https://laboscoc.org/housing-and-services/', 'LA', '1'),
+('Maine Housing', 'https://www.mainehousing.org/programs-services/homeless/emergency-shelters', 'ME', '1'),
+('Maryland Department of Housing and Community Development', 'https://dhcd.maryland.gov/HomelessServices/Pages/default.aspx', 'MD', '1'),
+('Massachusetts Department of Housing and Community Development', 'https://www.mass.gov/orgs/housing-and-community-development', 'MA', '1'),
+('Project Bread', 'https://www.projectbread.org/get-help', 'MA', '1'),
+('811 Project Rental Assistance Program', 'https://www.michigan.gov/mshda/homeless/811/811-project-rental-assistance-pra-program', 'MI', '1'),
+('NEMCSA Homeless and Prevention Services', 'https://www.nemcsa.org/services/homeless-prevention-program/', 'MI', '1'),
+('Disability Hub', 'https://mn.hb101.org/i/map/homeless_map.htm', 'MN', '1'),
+('Mississippi United to End Homelessness', 'https://www.muteh.org/', 'MS', '1'),
+('Missouri Department of Social Services', 'https://dss.mo.gov/fsd/esg/', 'MO', '1'),
+('St. Louis Homeless Services', 'https://www.stlouis-mo.gov/government/departments/human-services/homeless-services/Homeless-Resources.cfm', 'MO', '1'),
+('Poverello Center', 'https://www.thepoverellocenter.org/programs-and-services/', 'MT', '1'),
+('Nebraska Department of Health and Human Services: Homeless Assistance', 'https://dhhs.ne.gov/Pages/Homeless-Assistance.aspx#', 'NE', '1'),
+('Nevada Homeless Alliance', 'https://nevadahomelessalliance.org/gethelp/', 'NV', '1'),
+('NH DHHS: Shelter Services', 'https://www.dhhs.nh.gov/dcbcs/bhhs/shelter.htm', 'NH', '1'),
+('NJ Covid-19 Information Hub', 'https://covid19.nj.gov/faqs/nj-information/assistance-and-benefits/how-do-i-apply-for-food-cash-and-health-insurance-assistance-how-are-assistance-programs-adjusting-for-covid-19', 'NJ', '1'),
+('Housing New Mexico', 'https://housingnm.org/find-housing/emergency-shelter', 'NM', '1'),
+('Supportive Housing Coalition of New Mexico', 'https://www.shcnm.org/communities/', 'NM', '1'),
+('NY Coalition for the Homeless', 'https://www.coalitionforthehomeless.org/get-help/', 'NY', '1'),
+('NC Coalition to End Homelessness', 'https://www.ncceh.org/lookingforhelp/coordinatedassessment/', 'NC', '1'),
+('NC Homeless Education Program', 'https://hepnc.uncg.edu/', 'NC', '1'),
+('North Dakota Homeless Coalition', 'https://www.ndhomelesscoalition.org/resources', 'ND', '1'),
+('ND DHS: Resources for people who are homeless or at risk of homelessness', 'https://www.nd.gov/dhs/info/covid-19/housing-homelessness.html', 'ND', '1'),
+('Coalition on Homelessness and Housing in Ohio', 'http://cohhio.org/housing-information/', 'OH', '1'),
+('Ohio Legal Help', 'https://www.ohiolegalhelp.org/', 'OH', '1'),
+('Ohio Association of Foodbanks', 'https://ohiofoodbanks.org/foodbanks/', 'OH', '1'),
+('Heartline Oaklahoma', 'https://heartlineoklahoma.org/', 'OK', '1'),
+('Housing Oregon', 'https://housingoregon.org/resources-for-homelessness-services-covid-19-crisis/#', 'OR', '1'),
+('Portland Homeless Family Solutions', 'http://www.pdxhfs.org/housing', 'OR', '1'),
+('Interactive Map of Restrooms and Hygiene Stations', 'https://pdx.maps.arcgis.com/apps/webappviewer/index.html?id=3abca53221c64d6a9e35c5c8572a9696', 'OR', '1'),
+('Pennsylvania Youth Congress', 'https://payouthcongress.org/resources/emergency-housing/', 'PA', '1'),
+('Philadelphia Office of Homeless Services', 'http://philadelphiaofficeofhomelessservices.org/', 'PA', '1'),
+('Rhode Island Community Foodbank', 'https://rifoodbank.org/find-food/', 'RI', '1'),
+('Rhode Island Coalition to End Homelessness', 'https://www.rihomeless.org/help', 'RI', '1'),
+('SCIWAY', 'https://www.sciway.net/org/sc-homeless.html', 'SC', '1'),
+('Cornerstone Rescue Mission', 'http://www.cornerstonemission.org/', 'SD', '1'),
+('HUD Help Hotlines: South Dakota', 'https://www.hud.gov/states/south_dakota/homeless/hsghelp', 'SD', '1'),
+('Tennessee Department of Mental Health and Substance Abuse Services', 'https://www.tn.gov/behavioral-health/housing/additional-programs.html', 'TN', '1'),
+('TN State Plan to End Homelessness', 'https://www.tn.gov/content/dam/tn/mentalhealth/documents/TN_Homeless_Resources.pdf', 'TN', '1'),
+('TX HHS: Programs & Services', 'https://www.hhs.texas.gov/services/mental-health-substance-use/mental-health-crisis-services/programs-people-who-are-homeless-or-risk-becoming-homeless', 'TX', '1'),
+('The Road Home', 'https://theroadhome.org/', 'UT', '1'),
+('SLCO Crisis Information & Outreach', 'https://slco.org/homeless-services/resource-directory/crisis-information-and-outreach/', 'UT', '1'),
+('VT DCF Benefit Programs', 'https://dcf.vermont.gov/benefits', 'VT', '1'),
+('Vermont Coalition to End Homelessness', 'https://helpingtohousevt.org/', 'VT', '1'),
+('HUD Homeless Services Providers: VA', 'https://www.hud.gov/states/virginia/homeless/providers', 'VA', '1'),
+('WA Department of Commerce', 'https://www.commerce.wa.gov/serving-communities/homelessness/', 'WA', '1'),
+('Pierce County: Homeless Programs', 'https://www.piercecountywa.gov/3715/Homeless-Programs', 'WA', '1'),
+('Homeless Shelters Directory', 'https://www.homelessshelterdirectory.org/state/west_virginia', 'WV', '1'),
+('WV DHHR', 'http://www.wvdhhr.org/bph/tollfree.htm', 'WV', '1'),
+('WV DHHR: Family Assistance', 'https://dhhr.wv.gov/bcf/Services/familyassistance/Pages/default.aspx', 'WV', '1'),
+('WI Department of Children & Families: Homeless Services', 'https://dcf.wisconsin.gov/homeless', 'WI', '1'),
+('WI Department of Children & Families: Resources for Runaway and Homeless Youth', 'https://dcf.wisconsin.gov/map/rhy', 'WI', '1'),
+('Wyoming 211: Community Services', 'https://wy211.communityos.org/', 'WY', '1');
